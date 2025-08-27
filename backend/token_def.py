5cb999c08cc4f3ef5cf9d6281a8993f7d508b3d0
Chanuka Wijesinghe <140912680+oCHANKUo@users.noreply.github.com>
2025-08-27 13:46:30 +0530

    Update token_def.py

--- backend/token_def.py
+++ backend/token_def.py
@@ -1,1 +1,21 @@
+# You can change the tokens if you want. Or add more if you feel like it. But variable name format should be consistent
+
+TOKEN_ID      = "id"       # numbers or identifiers
+TOKEN_PLUS    = "plus"     # '+'
+TOKEN_MUL     = "mul"      # '*'
+TOKEN_MINUS   = "minus"    # '-'
+TOKEN_DIV     = "div"      # '/'
+TOKEN_LPAREN  = "lparen"   # '('
+TOKEN_RPAREN  = "rparen"   # ')'
+
+# a list of all token types
+ALL_TOKENS = [
+    TOKEN_ID,
+    TOKEN_PLUS,
+    TOKEN_MUL,
+    TOKEN_MINUS,
+    TOKEN_DIV,
+    TOKEN_LPAREN,
+    TOKEN_RPAREN
+]
 

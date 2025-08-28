d078ed57bf07f212954508c36a70c7f736cea744
Chanuka Wijesinghe <chanuka.s.wijesinghe@gmail.com>
2025-08-28 14:25:11 +0530

    Node Class

--- backend/parser.py
+++ backend/parser.py
@@ -1,1 +1,19 @@
 
+import ply.yacc as yacc
+from lexer import lex_input
+from token_def import ALL_TOKENS
+
+tokens = ALL_TOKENS
+
+# Node Class
+class Node:
+    def __init__(self, name, children=None, value=None):
+        self.name = name
+        self.children = children if children is not None else []
+        self.value = value
+
+    def __repr__(self):
+        if self.value is not None:
+            return f"{self.name}({self.value})"
+        else:
+            return f"{self.name}({self.children})"
